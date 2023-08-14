b85ae27c730a4b63805ed0336b3cb6dfcc46c142
mardon1dev <dusbekovmardonbek5430@gmail.com>
2023-08-14 22:40:06 +0500

    'ok'

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 
 const Category = () => {
+
+  const [data, setData] = useState();
+
+  useEffect(()=>{
+    const url = 'https://fakestoreapi.com/products';
+    fetch(url).then(response=>response.json()).then(data=>setData(data))
+  },[])
+
   return (
     <div className='category'>
         <div className='container'>
-            <div>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad saepe odit nemo dignissimos voluptates illo ducimus facere reprehenderit rerum assumenda.</p>
+            <div className='flex flex-cols-4'>
+                {
+                  data &&Object.keys(data).map((item, index)=>{
+                    return(
+                      <div key={index}>
+                        {/* <img src={`http://localhost:3001/${data[item].image}`} alt="product"/> */}
+                        <a href='#'><h2>{item.title}</h2></a>
+                        <p>{item.price} USD</p>
+                      </div>
+                    )
+                  })
+                }
             </div>
         </div>
     </div>
